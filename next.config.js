81d55b93e5c3deafbf520a4934b40aaac51b4dad
navneetty-netizen <navneetty@gmail.com>
2026-01-09 23:23:52 +0530

    export static site for github pages

---
+++ next.config.js
@@ -0,0 +1,9 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export',
+  images: { unoptimized: true },
+  basePath: '/Birthday-Globe-Website',
+  assetPrefix: '/Birthday-Globe-Website/',
+};
+
+module.exports = nextConfig;
